7f72beb7b7dcf471bcbc2c407c8c40426328275a
root <thomas@marguerit.at>
2019-11-04 22:44:12 +0100

    Modify Graphical Naval Menu and Naval Main

--- src/game_ressources/Score.java
+++ src/game_ressources/Score.java
@@ -1,22 +1,47 @@
 package game_ressources;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Score {
 
     /*
 
-    String scoreFile = "score.txt";
+    private String scoreFile = "score.txt";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Score score = new Score();
-        score.getScore("jean-kevin-du-93");
+        score.setScore("test", 42);
+        System.out.println(score.getScore("test"));
     }
 
-    private int getScore(String pseudo) {
+    public String[][] getAllScores() {
+        String[][] scores = new String[0][];
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(scoreFile));
+            String line = reader.readLine();
+            int index = 0;
+            while (line != null) {
+                String[] fileLine = line.split(";");
+                scores = new String[0][0];
+                scores = new String[0][1];
+                scores[1][0] = fileLine[0];
+                scores[index][1] = fileLine[1];
+                line = reader.readLine();
+                index++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return scores;
+    }
+
+    public int getScore(String pseudo) {
         BufferedReader reader;
         int result = 0;
-        String pseudoInFile;
         try {
             reader = new BufferedReader(new FileReader(scoreFile));
             String line = reader.readLine();
@@ -35,10 +60,22 @@ public class Score {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return result;
+    }
+
+    //---RECORDING OF THE PLAYER'S NEW SCORE---
+    //If the new score is higher than the recorded score : update the recorded score and return True.
+    //If the new score is less than or equivalent to the recorded score : not update the recorded score and return False.
+    public boolean setScore(String pseudo, int score) throws IOException {
+        String[][] scores = getAllScores();
+        System.out.println(Arrays.deepToString(scores));
+        FileWriter fw = new FileWriter(scoreFile, true);
+        fw.write("\n" + pseudo + ";" + score);
+        fw.close();
+        return true;
     }
 
 
      */
+
 }

0d1d220c8874f8b3af5857d33d4df05d34a4d88d
root <thomas@marguerit.at>
2019-11-05 20:17:43 +0100

    Modify Graphical Naval Menu and Naval Main

--- src/game_ressources/Score.java
+++ src/game_ressources/Score.java
@@ -1,24 +1,53 @@
 package game_ressources;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Score {
 
-    /*
-
     private String scoreFile = "score.txt";
 
-    public static void main(String[] args) throws IOException {
-        Score score = new Score();
-        score.setScore("test", 42);
-        System.out.println(score.getScore("test"));
+    //---INDICATES IF THE PLAYER'S SCORE IS GREATER THAN HIS PREVIOUS SCORE--
+    //If the new score is higher than the recorded score or if he doesn't have an old score: return True.
+    //If the new score is less than or equivalent to the recorded score : return False.
+    private boolean isNewScore(String pseudo, int scorePseudo, String[][] scores) {
+        boolean hasRecord = false;
+        for(String[] lineInFile : scores) {
+            //If the score belongs to the player of the new record
+            if(lineInFile[0].equals(pseudo)) {
+                //Saves the presence of an old score
+                hasRecord = true;
+                //If the new score is greater than the old score
+                if(Integer.parseInt(lineInFile[1]) < scorePseudo) {
+                    return true;
+                }
+            }
+        }
+        //If the player doesn't have an old score : return True.
+        //Else : return false;
+        return !(hasRecord);
     }
 
-    public String[][] getAllScores() {
-        String[][] scores = new String[0][];
+    //---GET THE NUMBER OF LINE IN A FILE---
+    private int numberLineInFile(String file) {
+        BufferedReader reader;
+        int numberLine = 0;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
+            while (line != null) {
+                numberLine++;
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return numberLine;
+    }
+
+    //---GET THE SCORE OF ALL PLAYERS---
+    //Return a double array ([0] : nickname and [1] : score).
+    private String[][] getAllScores() {
+        String[][] scores = new String[numberLineInFile(scoreFile)][2];
         BufferedReader reader;
         try {
             reader = new BufferedReader(new FileReader(scoreFile));
@@ -26,9 +55,7 @@ public class Score {
             int index = 0;
             while (line != null) {
                 String[] fileLine = line.split(";");
-                scores = new String[0][0];
-                scores = new String[0][1];
-                scores[1][0] = fileLine[0];
+                scores[index][0] = fileLine[0];
                 scores[index][1] = fileLine[1];
                 line = reader.readLine();
                 index++;
@@ -39,7 +66,8 @@ public class Score {
         return scores;
     }
 
-    public int getScore(String pseudo) {
+    //---GET THE PLAYER'S SCORE---
+    private int getScore(String pseudo) {
         BufferedReader reader;
         int result = 0;
         try {
@@ -63,19 +91,39 @@ public class Score {
         return result;
     }
 
-    //---RECORDING OF THE PLAYER'S NEW SCORE---
+    //---RECORD OF THE PLAYER'S NEW SCORE---
     //If the new score is higher than the recorded score : update the recorded score and return True.
     //If the new score is less than or equivalent to the recorded score : not update the recorded score and return False.
-    public boolean setScore(String pseudo, int score) throws IOException {
+    private boolean setScore(String pseudo, int score) throws IOException {
+        //Recovering old scores
         String[][] scores = getAllScores();
-        System.out.println(Arrays.deepToString(scores));
-        FileWriter fw = new FileWriter(scoreFile, true);
-        fw.write("\n" + pseudo + ";" + score);
-        fw.close();
-        return true;
+        if(isNewScore(pseudo, score, scores)) {
+            boolean firstRecord = true;
+            //Deleting the old content of the score file
+            PrintWriter writer = new PrintWriter(scoreFile);
+            writer.print("");
+            writer.close();
+            //Opening the score file for writing
+            FileWriter fw = new FileWriter(scoreFile, true);
+            //Browsing the old scores
+            for(String[] lineInFile : scores) {
+                //If the score belongs to the player of the new record, the old score is changed.
+                if(lineInFile[0].equals(pseudo)) {
+                    lineInFile[1] = String.valueOf(score);
+                    firstRecord = false;
+                }
+                //Saving the score (old or new) in the score file
+                fw.write(lineInFile[0] + ";" + lineInFile[1] + "\n");
+            }
+            //If the player does not have an old record, add a new line with his score.
+            if(firstRecord) {
+                fw.write(pseudo + ";" + score + "\n");
+            }
+            //Closing records file.
+            fw.close();
+            return true;
+        }
+        return false;
     }
-
-
-     */
 
 }

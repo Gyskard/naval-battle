bc95acd827835c1afb9a6c4f0077dae63098c756
root <thomas@marguerit.at>
2019-11-20 19:59:26 +0100

    Add IA_player

--- src/game_ressources/IA_Player.java
+++ src/game_ressources/IA_Player.java
@@ -1,26 +1,79 @@
 package game_ressources;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class IA_Player extends Players {
 
 	public IA_Player(int size , Players adversary) {
 		super(size);
 
-		/* For test */
-		/*
-
-		Graphic_Naval_Board adversaryBoard = new Graphic_Naval_Board(10, 957, 468, adversary);
-		adversaryBoard.randomBoatPositioning();
-
-		displayCells(adversaryBoard);
-		*/
-		/* Final code (pointer error) */
-
 		Graphic_Naval_Board adversaryBoard = adversary.getGameBoard();
-		Board_Cells[][] board_cells = adversaryBoard.getMyCells();
+
+        /* -- For test ---*/
+        adversaryBoard.randomBoatPositioning();
+        displayCells(adversaryBoard);
+        /* ---------------*/
+
+        List<Integer> xTested = new ArrayList<Integer>();
+        List<Integer> yTested = new ArrayList<Integer>();
+
+        tryToDestroy(adversaryBoard, xTested, yTested);
+        displayCells(adversaryBoard);
 
 	}
+
+	private boolean coordsHasBeenTested(List<Integer> xTested, List<Integer> yTested, int x, int y) {
+	    if(xTested.size() == 0 || yTested.size() == 0) {
+	        return false;
+        }
+	    else {
+            int i;
+	        for(i = 0; i < xTested.size(); i++) {
+	            if(xTested.get(i) == x && yTested.get(i) == y) {
+	                return true;
+                }
+            }
+        }
+	    return false;
+    }
+
+	private void tryToDestroy(Graphic_Naval_Board board, List<Integer> xTested, List<Integer> yTested) {
+
+        int x = 0;
+        int y = 0;
+
+        boolean boatInCase = false;
+        while(!(boatInCase)) {
+            boolean newCaseTest = false;
+            while(!(newCaseTest)) {
+                x = ThreadLocalRandom.current().nextInt(0, 9 + 1);
+                y = ThreadLocalRandom.current().nextInt(0, 9 + 1);
+                if(!(coordsHasBeenTested(xTested, yTested, x, y))) {
+                    newCaseTest = true;
+                }
+                xTested.add(x);
+                yTested.add(y);
+            }
+
+            if(boatInCase(board, x, y)) {
+                boatInCase = true;
+            }
+        }
+
+        System.out.println("\n\n===========================");
+        System.out.println(x);
+        System.out.println(y);
+
+        Board_Cells[][] board_cells = board.getMyCells();
+        board_cells[x][y].destroy();
+    }
+
+    private boolean boatInCase(Graphic_Naval_Board board, int x, int y) {
+        Board_Cells[][] board_cells = board.getMyCells();
+        return board_cells[x][y].getState();
+    }
 
 	private void displayCells(Graphic_Naval_Board board) {
 		Board_Cells[][] board_cells = board.getMyCells();
